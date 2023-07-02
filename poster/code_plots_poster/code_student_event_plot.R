25061bafd3ea15fdf272fed832397caa484263ff
jens.klenke <57100601+jens-klenke@users.noreply.github.com>
2023-07-02 20:11:50 +0200

    Poster update

--- poster/code_plots_poster/code_student_event_plot.R
+++ poster/code_plots_poster/code_student_event_plot.R
@@ -3,10 +3,10 @@ source(here::here('01_code/packages.R'))
 #load(here::here('00_data/plot_list.Rdata'))
 load(here::here('plot_list.Rdata'))
 
-save(log_20_21, L_1_20_21_av_0.2_index, clust_ID_20_21, points_20_21,
-     log_18_19, L1_18_19_av_0.2_index, clust_ID_18_19, points_18_19,
-     plot_list, plot_list_18_19, plot_list_20_21,
-     file = here::here('00_data/plot_list.Rdata'))
+# save(log_20_21, L_1_20_21_av_0.2_index, clust_ID_20_21, points_20_21,
+#      log_18_19, L1_18_19_av_0.2_index, clust_ID_18_19, points_18_19,
+#      plot_list, plot_list_18_19, plot_list_20_21,
+#      file = here::here('00_data/plot_list.Rdata'))
 
 # plot function 
 plot_cluster_stud <- function(data, point_data, clust, k) {
@@ -139,14 +139,6 @@ grid_plot_list <- function(plot_list, index){
 
 plot_height <- 10
 
-# Poster maybe later; B 
-plot_list_20_21[[2]]
-
-ggsave(filename = here::here('resources/graphics/plot_b_poster.png'), 
-       dpi = 1200,
-       height = plot_height, width = (21/9)*plot_height)
-
-
 # Plot AB
 cowplot::plot_grid(
   plot_list_20_21[[1]], NULL, plot_list_20_21[[2]],
@@ -247,5 +239,146 @@ cowplot::plot_grid(
 )
 
 ggsave(filename = "resources/graphics/plot_kl.png", 
+       dpi = 300,
+       height = plot_height, width = (21/9)*plot_height)
+
+
+#########################################################
+'Poster'
+#########################################################
+
+# change plot funktion for poster
+plot_cluster_stud_poster <- function(data, point_data, clust, k) {
+  limit <- range(data$timestamp)
+  
+  ex <- data %>% 
+    distinct(exercise)
+  
+  ID <- clust_ID[clust_ID == clust] %>%
+    labels()
+  
+  time_plot <- data %>%
+    dplyr::filter(STUDI_ID %in% ID) %>%
+    arrange(exercise) %>%
+    left_join(ex, by = "exercise") %>% 
+    ggplot(.) + 
+    geom_point(aes(y = timestamp, x = exercise, 
+                   fill = STUDI_ID, 
+                   color = STUDI_ID, 
+                   shape = STUDI_ID), 
+               alpha = 0.9, 
+               size = 2) + 
+    ylim(limit) +
+    ylab("Time \n") +
+    xlab("\n Task") +
+    theme_classic() +
+    theme(
+      plot.margin = unit(c(0, 0, 0.5, 0.5), "cm"),
+      panel.grid.major.x = element_line(),
+      text = element_text(size = 20), #from 10
+      axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = -0.02, size = 14), # here change
+      axis.text.y = element_text(size = 14), 
+      axis.title = element_text(size = 40),### from 8
+      legend.title = element_text(size = 28)) + ### from 8
+    scale_shape_manual(values = c(16, 5, 0, 6, 4), 
+                       labels = paste(LETTERS[k], 
+                                      as.character(1:2), 
+                                      sep = "."), 
+                       name = "Students", 
+                       na.translate = F) +
+    scale_colour_manual(values = ggthemes::colorblind_pal()(8)[c(2, 6, 4, 8, 5)], 
+                        labels = paste(LETTERS[k], 
+                                       as.character(1:2), 
+                                       sep = "."), 
+                        name = "Students", 
+                        na.translate = F) +
+    scale_fill_manual(values = ggthemes::colorblind_pal()(8)[c(2, 6, 4, 8, 5)], 
+                      labels = paste(LETTERS[k], 
+                                     as.character(1:2), 
+                                     sep = "."), 
+                      name = "Students", 
+                      na.translate = F) + 
+    scale_x_discrete(labels = c(
+      "1.4" = "1.3", 
+      "6a.1" = "6a", 
+      "6b.1" = "6b", 
+      "6c.1" = "6c", 
+      "7.1" = "7"
+    ))
+  
+  point_plot <- point_data %>%
+    dplyr::filter(STUDI_ID %in% ID) %>%
+    inner_join(ex, by = "exercise") %>%
+    arrange(exercise) %>%
+    ggplot() + 
+    geom_col(aes(x = exercise, 
+                 y = points, 
+                 fill = STUDI_ID), 
+             alpha = 0.9,
+             position = position_dodge(preserve = "single"), 
+             width = 0.8) +
+    ylab("Points") +
+    theme_classic() +
+    theme(
+      plot.margin = unit(c(2, -0.5, 0.2, 0), "cm"),
+      panel.grid.major.x = element_line(),
+      text = element_text(size = 10), 
+      axis.text.x = element_blank(), 
+      axis.text.y = element_text(size = 14),
+      axis.ticks.x = element_blank(),
+      axis.title.x = element_blank(),
+      axis.title.y = element_text(size = 40),###from 8
+      legend.title = element_text(size = 40)) + ### from 8
+    scale_fill_manual(
+      values = ggthemes::colorblind_pal()(8)[c(2, 6, 4, 8, 5)],
+      labels = paste(LETTERS[clust], 
+                     as.character(1:10), 
+                     sep = "."), 
+      name = "Students") + 
+    scale_x_discrete(labels = c(
+      "1.4" = "1.3", 
+      "6a.1" = "6a", 
+      "6b.1" = "6b", 
+      "6c.1" = "6c", 
+      "7.1" = "7"
+    )) +
+    scale_y_continuous(breaks = c(0, 100))
+  
+  
+  ggpubr::ggarrange(point_plot, 
+                    time_plot, 
+                    heights = c(0.75, 2.5), 
+                    ncol = 1, 
+                    common.legend = TRUE, 
+                    align = "v",
+                    legend = "right", 
+                    legend.grob = ggpubr::get_legend(time_plot))
+}
+
+
+
+
+clust_ID <- clust_ID_20_21
+
+plot_list_poster <- list()
+k <- 1
+for (i in L_1_20_21_av_0.2_index) {
+  plot_list_poster[[k]] <- log_20_21 %>%
+    plot_cluster_stud_poster(point_data = points_20_21,
+                             clust = i,
+                             k = k)
+  k <- k + 1
+}
+
+plot_height <- 10
+# Poster maybe later; B 
+plot_list_poster[2]
+
+# ggsave(filename = here::here('resources/graphics/plot_b_poster.png'), 
+#        dpi = 300,
+#        height = plot_height, width = (21/9)*plot_height)
+
+# svg try
+ggsave(filename = here::here('resources/graphics/plot_b_poster.svg'), 
        dpi = 300,
        height = plot_height, width = (21/9)*plot_height)
